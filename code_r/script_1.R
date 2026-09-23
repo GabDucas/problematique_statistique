fca22d9f4bcc1ccc6e411404944e0530d822006d
AntoineCosta <cosa5136@usherbrooke.ca>
2026-09-23 12:30:09 -0400

    traitement des donnes pour hypothese, tests tendance, shapiro

--- code_r/script_1.R
+++ code_r/script_1.R
@@ -18,6 +18,7 @@ df_global <- map_dfr(fichiers, read_table, .id = "fichier")
 
 # Extraire les donnees des titres de fichiers
 df_global <- df_global |>
+  group_by(fichier) |>
   mutate(
     # Extrait les chiffres situés après "Poids" et avant "Kg"
     poids_kg = as.numeric(str_extract(fichier, "(?<=Poids)[0-9.]+")),
@@ -26,5 +27,60 @@ df_global <- df_global |>
     grandeur_m = as.numeric(str_extract(fichier, "(?<=Grandeur)[0-9.]+")),
     
     # Extraire la date situee entre les deux _
-    date = str_extract(fichier, "(?<=_)[^_]+(?=_Poids)")
+    date = str_extract(fichier, "(?<=_)[^_]+(?=_Poids)"),
+    
+    # Extraire angle max
+    angle_max = max(abs(angle), na.rm = TRUE),
+    
+    # Temps stabilisation
+    temps_stabilisation = {
+      # 1. Trouver toutes les lignes où l'angle est hors de l'intervalle cible
+      idx_hors_limites <- which(abs(angle) > 2)
+      
+      # 2. Logique conditionnelle pour isoler le moment de stabilisation
+      if (length(idx_hors_limites) == 0) {
+        # Cas A : L'angle n'a jamais dépassé 2 (stable dès le départ)
+        min(temps, na.rm = TRUE)
+        
+      } else if (max(idx_hors_limites) == n()) {
+        # Cas B : La toute dernière valeur du fichier est encore hors limite 
+        # (Le système ne s'est jamais stabilisé dans le temps imparti)
+        NA_real_
+        
+      } else {
+        # Cas C : Trouver le temps (t) à la ligne qui suit exactement la dernière instabilité
+        temps[max(idx_hors_limites) + 1]
+      }
+    }
+  ) |>
+  ungroup()
+
+resume_essais <- df_global |>
+  group_by(fichier) |>
+  summarise(
+    poids_kg = first(poids_kg),
+    grandeur_m = first(grandeur_m),
+    angle_max = first(angle_max),
+    temps_stabilisation = first(temps_stabilisation)
   )
+
+# Modéliser l'impact combiné du poids et de la grandeur sur l'angle maximum
+modele_temps <- lm(temps_stabilisation ~ poids_kg + grandeur_m, data = resume_essais)
+
+# Afficher les résultats complets
+summary(modele_temps)
+
+# Modéliser l'impact combiné du poids et de la grandeur sur l'angle maximum
+modele_angle <- lm(angle_max ~ poids_kg + grandeur_m, data = resume_essais)
+
+# Afficher les résultats complets
+summary(modele_angle)
+
+# Shapiro pour la normalite des donnees (elles ne le sont pas)
+shapiro.test(resume_essais$angle_max)
+shapiro.test(resume_essais$temps_stabilisation)
+shapiro.test(resume_essais$grandeur_m)
+shapiro.test(resume_essais$poids_kg)
+
+# Test a faire : Test en t
+# t.test(balls)
